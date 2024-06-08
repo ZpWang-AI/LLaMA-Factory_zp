8412406f8b71cd30387c26dab3d85d3dc2b8ffa5
cipolee <1633272721@qq.com>
2024-06-08 12:25:43 +0800

    modified:   src/build_dataset.py 	modified:   src/utils

--- src/build_dataset.py
+++ src/build_dataset.py
@@ -5,18 +5,25 @@ from typing import *
 from copy import deepcopy
 from pathlib import Path as path
 
-from IDRR_data import DataFrames, DataFrames2, PromptFiller
-from utils import AttrDict, dump_json, load_json
+from IDRR_data import IDRRDataFrames, PromptFiller
+from utils import AttrDict, ExpArgs, dump_json, load_json
 
 
-class BuildDataset(AttrDict):
+class BuildDataset(ExpArgs):
     def __init__(self) -> None:
+        # ========== 'data' ========================
+        self.part1 = 'data'
         self.data_name = 'pdtb3'
-        self.label_level = 'level1'
-        self.relation = 'Implicit'
+        self.data_level = 'level1'
+        self.data_relation = 'Implicit'
+
+        # ========== 'path' ========================
+        self.part2 = 'path'
         self.data_path = ''
         self.llama_factory_dir = '/home/qwe/test/zpwang/LLaMA/LLaMA-Factory'
         
+        # ========== 'base setting' ================
+        self.part3 = 'base setting'
         self.prompt = {
             "instruction": "Figure out the relation between the pair of arguments. The answer should be one of (Expansion, Temporary, Contingency and Comparison).",
             "input": 'The first argument is\n\n{arg1}\n\nThe second argument is\n\n{arg2}',
@@ -28,31 +35,33 @@ class BuildDataset(AttrDict):
             "history": []
         }
         self.desc = 'test'
-        
         self.max_seq_length = 2048
+        
+        # ========== 'additional info' =============
+        self.part4 = 'additional info'
         self.set_create_time()
 
     @property
     def version(self):
         info_list = [
             self.data_name,
-            self.label_level,
+            self.data_level,
             self.create_time,
             self.desc,
             f'clip{self.max_seq_length}'
         ]
         return '.'.join(info_list).replace('-', '_')
-
-    def dump_json(self, processed_data_name):
-        json_path = path('/public/home/hongy/zpwang/LLaMA-Factory/data/dataset_args')
-        json_path /= f'{processed_data_name}.json'
-        self._dump_json(json_path=json_path, overwrite=True)
     
     def start(self):
-        dataframes = DataFrames2(
+        self.data_path = path(self.data_path)
+        self.llama_factory_dir = path(self.llama_factory_dir)
+        assert self.data_path.exists()
+        assert self.llama_factory_dir.exists()
+        
+        dataframes = IDRRDataFrames(
             data_name=self.data_name,
-            label_level=self.label_level,
-            relation=self.relation,
+            data_level=self.data_level,
+            data_relation=self.data_relation,
             data_path=self.data_path,
         )
         
@@ -60,30 +69,31 @@ class BuildDataset(AttrDict):
         eval_prompt = self.prompt['eval'] if 'eval' in self.prompt else self.prompt
 
         self.build_single_dataset(
-            processed_data=list(PromptFiller(
+            processed_data=PromptFiller(
                 df=dataframes.train_df,
                 prompt=train_prompt,
-            )),
-            processed_data_name=self.version+'_train'
+            ).list,
+            processed_data_name=self.version+'.train'
         )
         
         for split in 'dev test'.split():
             self.build_single_dataset(
-                processed_data=list(PromptFiller(
+                processed_data=PromptFiller(
                     df=dataframes.get_dataframe(split=split),
                     prompt=eval_prompt,
-                )),
-                processed_data_name=self.version+'_'+split
+                ).list,
+                processed_data_name=f'{self.version}.{split}'
             )
         
         build_dataset_info_path = path(self.llama_factory_dir)/'data'/'build_dataset_info.json'
-        build_dataset_info = load_json(build_dataset_info)
-        build_dataset_info[self.version] = self.to_json()
-        dump_json(build_dataset_info)
-        # self.dump_json(processed_data_name)
-        
+        if build_dataset_info_path.exists():
+            build_dataset_info = load_json(build_dataset_info)
+        else:
+            build_dataset_info = {}
+        build_dataset_info[self.version] = self.json
+        dump_json(build_dataset_info, build_dataset_info_path, mode='w', indent=4)
             
-    def build_single_dataset(self, processed_data, processed_data_name):
+    def build_single_dataset(self, processed_data:List[Dict[str, str]], processed_data_name):
         if self.max_seq_length:
             def clip_func(piece_of_data):
                 for k, v in piece_of_data.items():
@@ -96,20 +106,18 @@ class BuildDataset(AttrDict):
             print(processed_data_name, 'has no data')
             return
         
-        target_file = f'/public/home/hongy/zpwang/LLaMA-Factory/data/{processed_data_name}.json'
-        with open(target_file, 'w', encoding='utf-8')as f:
-            json.dump(processed_data, f, ensure_ascii=True, indent=2)
+        target_file = self.llama_factory_dir/'data'/f'{processed_data_name}.json'
+        dump_json(processed_data, target_file, mode='w', indent=2)
         
-        dataset_info_file = path('/public/home/hongy/zpwang/LLaMA-Factory/data/dataset_info.json')
-        if dataset_info_file.exists():
-            with open(dataset_info_file, 'r', encoding='utf8')as f:
-                dataset_info = json.load(f)
+        dataset_info_path:path = self.llama_factory_dir/'data'/'dataset_info.json'
+        if dataset_info_path.exists():
+            dataset_info = load_json(dataset_info_path)
         else:
             dataset_info = {}
         
         if processed_data_name not in dataset_info:
             dataset_info[processed_data_name] = {
-                "file_name": target_file,
+                "file_name": str(target_file),
                 "columns": {
                     "prompt": "instruction",
                     "query": "input",
@@ -118,11 +126,12 @@ class BuildDataset(AttrDict):
                     "history": "history"
                 }
             }
-            with open(dataset_info_file, 'w', encoding='utf8')as f:
-                json.dump(dataset_info, f, indent=4)
+            dump_json(dataset_info, dataset_info_path, mode='w', indent=4)
             print('add data:', processed_data_name, '\n')
         else:
             print(processed_data_name, 'exists')
 
 # arg1 arg2 conn1 conn2 
 # conn1sense1 conn1sense2 conn2sense1 conn2sense2
+
+# BuildDataset.format_part_in_file(__file__)
