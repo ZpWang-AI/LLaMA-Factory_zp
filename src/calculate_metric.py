ff687a12a87d5b11a3254fddf6694eed84c3485d
jiah-li <lijiahuiim@gmail.com>
2024-06-22 11:55:50 +0800

    modified:   src/calculate_metric.py

--- src/calculate_metric.py
+++ src/calculate_metric.py
@@ -3,6 +3,7 @@ from utils_zp.common_import import *
 from sklearn.metrics import (
     classification_report,
     precision_recall_fscore_support, 
+    confusion_matrix
 )
 
 from utils_zp import postprocess_generation_res_to_lid, load_json, dump_json
@@ -26,18 +27,42 @@ def calculate_metric(target_dir):
     label_list = postprocessed['label_list']
 
     # TODO
+    confusion_mat = confusion_matrix(
+        y_true=gt, y_pred=pred,
+        labels=list(range(len(label_list))),
+    )
     cls_report = classification_report(
         y_true=gt, y_pred=pred, 
         labels=list(range(len(label_list))), 
         target_names=label_list, zero_division=0,
         output_dict=True,
     )
+    print(confusion_mat)
+    print_sep()
+    print(classification_report(
+        y_true=gt, y_pred=pred, 
+        labels=list(range(len(label_list))), 
+        target_names=label_list, zero_division=0,
+        output_dict=False,
+    ))
     res_dic = {
         'macro-f1': cls_report['macro avg']['f1-score'],
-        'res_dic': json.dumps(cls_report, ensure_ascii=False, indent=4)
+        'yes_precision': cls_report['yes']['precision'],
+        'confusion_matrix': confusion_mat.tolist(),
+        'cls_report': cls_report,
     }
     dump_json(res_dic, target_dir/'processed_dic.json', indent=4)
 
 
 if __name__ == '__main__':
-    calculate_metric('/home/qwe/test/zpwang/LLaMA/exp_space/base/2024-06-14-10-32-07.base_pred.ckpt4000.bs1*8_lr0.0001_ep5')
+    for dir in '''
+    /home/qwe/test/zpwang/LLaMA/exp_space/filter/2024-06-21-20-37-57.filter.ckpt2000.bs1*8_lr0.0001_ep5
+    /home/qwe/test/zpwang/LLaMA/exp_space/filter/2024-06-21-21-57-37.filter.ckpt4000.bs1*8_lr0.0001_ep5
+    /home/qwe/test/zpwang/LLaMA/exp_space/filter/2024-06-22-09-54-06.filter.ckpt6000.bs1*8_lr0.0001_ep5
+    /home/qwe/test/zpwang/LLaMA/exp_space/filter/2024-06-22-09-54-36.filter.ckpt8000.bs1*8_lr0.0001_ep5
+    /home/qwe/test/zpwang/LLaMA/exp_space/filter/2024-06-22-09-55-00.filter.ckptfinal.bs1*8_lr0.0001_ep5
+    '''.split():
+        dir = dir.strip()
+        if dir:
+            calculate_metric(dir)
+    # calculate_metric('/home/qwe/test/zpwang/LLaMA/exp_space/filter/2024-06-21-20-37-57.filter.ckpt2000.bs1*8_lr0.0001_ep5')
