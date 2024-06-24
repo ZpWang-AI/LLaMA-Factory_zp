d359de3a544926a0aa750b6c1bd341af8e0e21cc
jiah-li <lijiahuiim@gmail.com>
2024-06-24 14:20:12 +0800

    modified:   src/template_script/dataset_script.py

--- src/template_script/dataset_script.py
+++ src/template_script/dataset_script.py
@@ -20,17 +20,8 @@ class PromptMaker:
                 # ["The first argument is\n\nBach's \"Air\" followed\n\nThe second argument is\n\nMr. Stoltzman tied the composer in by proclaiming him \"the great improviser of the 18th century", 'Temporal'],
             ]
         }
-        pred_prompt = {
-            "instruction": 'Argument 1:\n{arg1}\n\nArgument 2:\n{arg2}\n\nQuestion: What is the discourse relation between Argument 1 and Argument 2?\nA. Comparison\nB. Contingency\nC. Expansion\nD. Temporal\n\nAnswer:',
-            "input": "",
-            "output": "{data_id}",
-            "system": "",
-            # "system": "The task is to determine whether they have a temporal, comparative, contingency, or extensional relationship. This analysis should consider both implicit and explicit relationships.",
-            "history": [
-                # ['The first argument is\n\nHis recent appearance at the Metropolitan Museum, dubbed \"A Musical Odyssey,\" was a case in point\n\nThe second argument is\n\nIt felt more like a party, or a highly polished jam session with a few friends, than a classical concert', 'Expansion'],
-                # ["The first argument is\n\nBach's \"Air\" followed\n\nThe second argument is\n\nMr. Stoltzman tied the composer in by proclaiming him \"the great improviser of the 18th century", 'Temporal'],
-            ]
-        }
+        pred_prompt = train_prompt
+        pred_prompt['output'] = '{data_id}'
         return {'train': train_prompt, 'pred': pred_prompt}
         
     
